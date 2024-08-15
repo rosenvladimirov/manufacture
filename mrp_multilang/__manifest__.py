16a410a13b03742acc506e3b48dd5403ea76a215
rosenvladimirov <vladimirov.rosen@gmail.com>
2024-08-15 16:25:01 +0300

    Add MRP Multilang module
    Introduced a new MRP Multilang module to support multilingual fields in manufacturing work centers and routings. This includes necessary imports and model definitions for translation functionality.

---
+++ mrp_multilang/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Rosen Vladimirov
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'MRP Multilang',
+    'description': """
+        Multilanguage support for manufacture work centers.""",
+    'version': '17.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Rosen Vladimirov,Odoo Community Association (OCA)',
+    'depends': [
+        'mrp',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
